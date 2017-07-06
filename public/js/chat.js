a0c5bd1cb6c66b49036bd40a99acd246b095a2f5
Tom Elliott <tom.w.elliott@gmail.com>
2017-07-05 22:02:08 -0400

    Expand message and user boxes to fill available space and automatically scroll to the bottom of the message div as messages are added.

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,6 +13,7 @@ $(function() {
 
   var documentId = $chat.data("document");
   var $messages = $chat.find('#messages');
+  $messages.text("");
 
   var $nameForm = $chat.find('#change_name');
   var $nameDisplay = $chat.find('#name_display');
@@ -29,14 +30,27 @@ $(function() {
 
   var nextMessage = 0;
 
+  var addMessages = function(messages) {
+    var atBottom = $messages.text().length == 0 || $messages.prop('scrollHeight') == $messages.scrollTop() + $messages.prop('clientHeight');
+    for (j in messages) {
+      var mes = messages[j];
+      var name = sessionNames[mes.SessionID];
+      if (!name) {
+        name = mes.SessionID;
+      }
+      $messages.append('<p><strong>' + name + '</strong>: ' + mes.Message + '</p>');
+    }
+    if (atBottom) {
+      $messages.animate({ scrollTop: $messages.prop("scrollHeight")}, 200);
+    }
+  };
+
   var updateUsers = function() {
     $currentName.text(sessionNames[sessionId]);
     $users.text("");
-    $users.append("<ul>");
     for (id in sessionNames) {
-      $users.append("<li>" + sessionNames[id] + "</li>");
+      $users.append("<p>" + sessionNames[id] + "</p>");
     }
-    $users.append("</ul>");
   };
   updateUsers();
 
@@ -99,20 +113,17 @@ $(function() {
           pollUpdates();
           return;
         }
+        var messages = [];
         for (i in events) {
           var ev = events[i];
-          for (j in ev.messages) {
-            var mes = ev.messages[j];
-            var name = sessionNames[mes.SessionID];
-            if (!name) {
-              name = mes.SessionID;
-            }
-            $messages.append('<p><strong>' + name + '</strong>: ' + mes.Message + '</p>');
-          }
+          messages = messages.concat(ev.messages);
           for (key in ev.sessionNameChanges) {
             var value = ev.sessionNameChanges[key];
             sessionNames[key] = value;
           }
+        }
+        if (messages.length > 0) {
+          addMessages(messages);
         }
         updateUsers();
         nextMessage+=events.length;
